56c2c44ec43383aa4d2cb97882af0592a03161ae
SM Abu Taher Asif <shayokhbd144@gmail.com>
2017-08-29 11:48:46 +0600

    Update ConnectionTest.py

--- ConnectionTest.py
+++ ConnectionTest.py
@@ -6,7 +6,7 @@ Created on Tue Aug 29 09:55:22 2017
 """
 
 import MySQLdb
-connection=MySQLdb.connect(host="localhost", user="root", passwd="", db="mysql")
+connection=MySQLdb.connect(host="host_name", user="user_name", passwd="your_password", db="database_name")
 cur=connection.cursor() 
 cur.execute("select Host, User, Password from user") 
 multiplerow=cur.fetchall()
